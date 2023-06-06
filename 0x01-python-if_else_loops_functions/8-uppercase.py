1dfc37362baf3cb01fe3e83bf270fecbb5d617d8
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-06 20:50:04 +0000

    solved check lower case and convert into uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for a in str:
+        b = ord(a)
+        if b >= 97 and b <= 122:
+            c = chr(b - 32)
+        else:
+            c = a
+        print(c, end='')
+    print()
