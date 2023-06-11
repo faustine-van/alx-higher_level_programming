73bd296dcc23ff80ccaa6c4a412aef4d37c97945
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-11 19:48:29 +0000

    first program to print integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,6 +2,5 @@
 
 def print_list_integer(my_list=[]):
     for a in range(len(my_list)):
-        if (all(isinstance(elem, int) for elem in my_list)):
             print("{:d}".format(my_list[a]))
 # instead of instance use print("{:d}".format(my_list[a]))
