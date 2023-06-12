dd59e41c540d53bd0b594a71c4d6dcd6698d9658
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-12 10:35:07 +0000

    updated print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for a in matrix:
-        for j in a:
-            print("{}".format(j), end=" ")
+        for r, j in enumerate(a):
+            print("{:d}".format(j), end="")
+            if r != len(a) - 1:
+                print(" ", end="")
         print()
