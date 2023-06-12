0989c4ea54303e3cdd702234b504130470b5787c
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-12 07:15:21 +0000

    fixed out of range in list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,7 +4,8 @@ def delete_at(my_list=[], idx=0):
         return my_list
     if idx < 0:
         return my_list
-    for a in my_list:
+# use enumerate to access both element and index
+    for a, x in enumerate(my_list):
         if a == idx:
             del my_list[idx]
     return my_list
