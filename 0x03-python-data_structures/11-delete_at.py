7639671b18d12937d7e13b5a6223c5ff728fbb11
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-12 07:08:08 +0000

    fixed out of range in list

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if idx > len(my_list):
+    if idx >= len(my_list):
+        return my_list
+    if idx < 0:
         return my_list
     for a in my_list:
-        if idx == a:
+        if a == idx:
             del my_list[idx]
     return my_list
