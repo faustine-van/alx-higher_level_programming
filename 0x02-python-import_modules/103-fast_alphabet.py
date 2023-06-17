6aac4255d8dffd6f42add08ed3836f51513627aa
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-17 07:35:57 +0000

    fast_alphabets

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-import string
-print(getattr(string, 'ascii_uppercase'))
+print("\101\102\103\104\105\106\107\110\111\112\112\113\114\125\115", end="")
+print("\116\117\120\121\122\123\124\125\126\127\130\131\132")
