59e96d92457d18a218ca845fd413bb3c793a557e
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-17 21:29:18 +0000

    updated

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-# check using isinstance() or type() function
+    # check using isinstance() or type() function
     if not type(roman_string) == str or roman_string is None:
         return 0
     roman = roman_string
