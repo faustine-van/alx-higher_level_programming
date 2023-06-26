99103e067ae554d35ee5c9eaa2757ef17a031a09
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-26 20:36:39 +0000

    updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     a = 0
+    count = 0
     while a < x:
         try:
             print(my_list[a], end="")
             a += 1
+            count += 1
         except IndexError:
             break
     print()
-    return x
+    return a
