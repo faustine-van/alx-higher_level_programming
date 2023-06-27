d772313a3a3dab2458b81f055e4ab892457d050c
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-27 14:37:02 +0000

    updated

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,12 +1,4 @@
-#!/usr/bin/python3
-
-"""This module is square class"""
-
-
 class Square:
-
-    """This square class that defined by Square"""
-
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
@@ -17,11 +9,13 @@ class Square:
 
     @size.setter
     def size(self, value):
-        self.__size = value
-        if not isinstance(value, int):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
             raise TypeError("size must be an integer")
-        if value < 0:
-            raise ValueError("size must be >= 0")
 
     @property
     def position(self):
@@ -32,21 +26,19 @@ class Square:
         self.__position = value
         if (
             not isinstance(value, tuple)
-            and not len(value) == 0
+            and not len(value) == 2
             and not all(isinstance(x, int) and x > 0 for x in value)
            ):
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
-        """public instance method that  calculate area with size argumebt"""
-        area = self.__size ** 2
-        return area
+        return self.size ** 2
 
     def my_print(self):
-        """print the #  and space with size and position"""
-        if self.__size == 0:
+        if self.size == 0:
             print()
-        for _ in range(self.position[1]):
-            print()
-        for _ in range(self.size):
-            print(" " * self.position[0] + "#" * self.size)
+        else:
+            for _ in range(self.position[1]):
+                print()
+            for _ in range(self.size):
+                print(" " * self.position[0] + "#" * self.size)
