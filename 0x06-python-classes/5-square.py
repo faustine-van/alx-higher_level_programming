83bdc370c38f2c8f398b844a7616eb70e7d95d3a
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-27 11:14:01 +0000

    classes and objects

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -27,6 +27,7 @@ class Square:
         return area
 
     def my_print(self):
+        """prints in stdout the square with the character #"""
         if self.__size == 0:
             print()
         for _ in range(self.size):
