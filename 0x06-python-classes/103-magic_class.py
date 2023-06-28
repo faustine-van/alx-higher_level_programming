7a9de1c5edaebaf057b9b37d10696b3536b1ebd0
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-28 22:31:35 +0000

    fixed int and float

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+
 """this module to take  python bytecode to
    python class called MagicClass
 """
+
 import math
 
 
 class MagicClass:
     """this class is for calculate area and circumference"""
+
     def __init__(self, radius):
         """initialize radius
 
            Argument:
                radius : which float or int
         """
+
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
@@ -20,8 +24,10 @@ class MagicClass:
 
     def area(self):
         """ calculate area of circle"""
+
         return (self.__radius ** 2 * math.pi)
 
     def circumference(self):
         """calculate circumference of circle"""
+
         return (2 * math.pi * self.__radius)
