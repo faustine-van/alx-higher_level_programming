02ce4403df2d2bc9dcab0361495b6f6987ed0b07
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-28 07:07:07 +0000

    fixed exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-        if not isinstance(value, int):
-            raise TypeError()
         return True
-    except Exception as e:
-        print(e, file=sys.stderr)
+    except (TypeError, ValueError) as e:
+        message_error = "Exception: " + str(e)
+        print(message_error, file=sys.stderr)
         return False
