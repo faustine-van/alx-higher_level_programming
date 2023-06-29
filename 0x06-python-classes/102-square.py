f3e631a8ec44a7b82088f75be718ec3af24b6022
faustine-van <faustinemuhayemariya44@gmail.com>
2023-06-29 11:19:27 +0000

    comparators: ==, !=, >, >=, < and <= based on the square area

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""This module  class Square that defines a square"""
+
+
+class Square:
+    """This Square class that defines a square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """function create area of square"""
+        area = self.__size ** 2
+        return area
+
+    def __lt__(self, other):
+        return (self.area()) < (other.area())
+
+    def __gt__(self, other):
+        return (self.area()) > (other.area())
+
+    def __eq__(self, other):
+        return (self.area()) == (other.area())
+
+    def __ge__(self, other):
+        return (self.area()) >= (other.area())
+
+    def __le__(self, other):
+        return (self.area()) <= (other.area())
+
+    def __ne__(self, other):
+        return not self == other
