bc135370c78181036e601f39f0dc11886e6f8ddc
faustine-van <faustinemuhayemariya44@gmail.com>
2023-07-12 21:33:08 +0000

    updated

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""module"""
+
 
 def add_attribute(a, name, value):
+    """function add new attributes"""
     if not hasattr(a, "__dict__"):
         raise TypeError("can't add new attribute")
     else:
