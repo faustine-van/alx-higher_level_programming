141a3d38ee3a0d522bd9d76d95283a35c9bdbc87
faustine-van <faustinemuhayemariya44@gmail.com>
2023-07-12 11:10:06 +0000

    check an instance of object directly or indirectly

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,7 +8,7 @@ def inherits_from(obj, a_class):
         otherwise False.
     """
 
-    if isinstance(obj, a_class):
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
         return False
