04c6a164d80478703d2bab6496f28e436ae722f4
faustine-van <faustinemuhayemariya44@gmail.com>
2023-07-12 13:41:49 +0000

    updated

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module create empyty class"""
+
+
+class BaseGeometry:
+    """ empty class BaseGeometry
+    """
+    def area(self):
+        """ area() method
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+
+        """ def integer_validator() method
+
+            args:
+                 name: name
+                 value: value
+            Raises:
+                  - if value is not an integer
+                  - if value is less or equal to 0
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
