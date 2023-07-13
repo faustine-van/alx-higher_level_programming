57efd7bca8a53427f059ed7dcd89a43f015be58a
faustine-van <faustinemuhayemariya44@gmail.com>
2023-07-13 19:05:47 +0000

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,6 +9,16 @@ args = sys.argv
 argc = len(sys.argv)
 new_list = []
 
+try:
+    # to loads in order to be able to append to the list
+    existing_list = load_from_json_file("add_item.json")
+
+    # adds the specified list elements to the end of the current list.
+    new_list.extend(existing_list)
+except FileNotFoundError: # this create file if doesn't exist
+    pass
+
 for i in range(1, argc):
     new_list.append(args[i])
+# and the convert the into json
 save_to_json_file(new_list, "add_item.json")
