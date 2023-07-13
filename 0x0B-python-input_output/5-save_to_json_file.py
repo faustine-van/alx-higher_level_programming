fbadb142f7bb7227f4a4f1c99f3a0037ffd9dcf5
faustine-van <faustinemuhayemariya44@gmail.com>
2023-07-13 11:25:24 +0000

    save object to file using json represenation

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """write an object Python data structure using JSON representation"""
+    with open(filename, 'w', encoding="utf-8") as file:
+        json_data = json.dumps(my_obj)
+        file.write(json_data)
