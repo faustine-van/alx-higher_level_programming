a3d2200cf61d7070140c5e505294775ba6a6ede6
faustine-van <faustinemuhayemariya44@gmail.com>
2023-07-13 12:06:17 +0000

    creates an Object from a “JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module"""
+import json
+
+
+def load_from_json_file(filename):
+    """function that creates an Object from a “JSON file"""
+    with open(filename, 'r', encoding="utf-8") as file:
+        content = file.read()
+        return json.loads(content)
