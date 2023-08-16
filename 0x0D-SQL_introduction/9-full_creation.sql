014babed926f1e3c714c150e95545fd0a6d2ed8e
faustine-van <faustinemuhayemariya44@gmail.com>
2023-08-16 18:21:05 +0000

    updated

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,7 +7,7 @@ name VARCHAR(256),
 score int
 );
 INSERT INTO second_table VALUES
-(1, 'John', 10),
-(2, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8);
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
