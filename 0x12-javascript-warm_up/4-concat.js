21e582044e8e5b9c991204219e1182e28acfba4b
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-11 18:21:41 +0000

    updated

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* prints two arguments passed to it, in the following format: “ is ” */
+
+const process = require('process');
+const args = process.argv;
+
+console.log(args[2] + ' is ' + args[3]);
