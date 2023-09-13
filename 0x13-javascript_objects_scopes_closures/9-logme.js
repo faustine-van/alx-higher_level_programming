0665758e6bfae1c8c0fe7e9b3e3ac6d5dd9e9446
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-13 19:22:56 +0000

    updated

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+ - prints the number of arguments already printed and the new argument value
+ - Prototype: exports.logMe = function (item)
+ - Output format: <number arguments already printed>: <current argument value>
+ */
+let count = 0;
+
+function logMe (item) {
+  for (let a = 0; a < arguments.length; a++) {
+    console.log(count + ':' + arguments[a]);
+    // console.log('${count}:${item});
+  }
+  count++;
+}
+module.exports.logMe = logMe;
