6ef0af84d47b5ed0c619acfcfbee6be3895da4dd
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-13 19:54:59 +0000

    updated

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,11 @@
 //  imports an array and computes a new array
 const list = require('./100-data.js').list;
 
+// Create a new array with the same values as the original
 const newArray = list.map((a) => a);
 console.log(newArray);
-const newArray1 = list.map((a) => a * list.indexOf(a));
+/* Create a new array where each element is multiplied by its index
+   in the original array
+*/
+const newArray1 = list.map((a, index) => a * index);
 console.log(newArray1);
