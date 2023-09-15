f5ffbd4c49640703690a04b11482b0aee997c397
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-15 09:02:10 +0000

    updated

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+- script that takes in the name of a state as an argument and lists all cities
+  of that state, using the database hbtn_0e_4_usa(SQL injection free)
+script should take 3 arguments: mysql username,mysql password and database name
+use the module MySQLdb (import MySQLdb)
+script should connect to a MySQL server running on localhost at port 3306
+
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    # Connect to the MySQL server
+    connection = MySQLdb.connect(
+        host="localhost", port=3306, user=args[1], passwd=args[2],
+        db=args[3], charset="utf8")
+
+    # Create a cursor object
+    cur = connection.cursor()
+
+    # Execute the SQL query to select states with names starting with 'N'
+    query = "SELECT cities.name FROM cities\
+        JOIN states ON cities.state_id = states.id WHERE states.name = %s"
+    cur.execute(query, (args[4],))
+
+    # Fetch all the rows and display the results
+    queryRows = cur.fetchall()
+    # print(queryRows[-1])
+    for row in queryRows:
+        print(row[0], end=", " if row != queryRows[-1] else "\n")
+
+    # Close the cursor and the database connection
+    cur.close()
+    connection.close()
