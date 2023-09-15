ad68661b39e473a0aa25941ecd6d3b72ccb68d5c
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-15 07:36:35 +0000

    updated

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,13 +12,23 @@ import sys
 
 if __name__ == "__main__":
     args = sys.argv
+
+    # Connect to the MySQL server
     connection = MySQLdb.connect(
         host="localhost", port=3306, user=args[1], passwd=args[2],
         db=args[3], charset="utf8")
+
+    # Create a cursor object
     cur = connection.cursor()
+
+    # Execute the SQL query to select states with names starting with 'N'
     cur.execute("SELECT * FROM states WHERE name = '{}'".format(args[4]))
+
+    # Fetch all the rows and display the results
     queryRows = cur.fetchall()
     for row in queryRows:
         print(row)
+
+    # Close the cursor and the database connection
     cur.close()
     connection.close()
