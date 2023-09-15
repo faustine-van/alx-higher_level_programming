39539336d01764d85bdc5387715e2fd8328fa717
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-15 08:12:44 +0000

    updated

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+- takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+script should take 3 arguments: mysql username,mysql password and database name
+use the module MySQLdb (import MySQLdb)
+script should connect to a MySQL server running on localhost at port 3306
+
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    # Connect to the MySQL server
+    connection = MySQLdb.connect(
+        host="localhost", port=3306, user=args[1], passwd=args[2],
+        db=args[3], charset="utf8")
+
+    # Create a cursor object
+    cur = connection.cursor()
+
+    # Execute the SQL query to select states with names starting with 'N'
+    query = "SELECT cities.id, cities.name, states.name FROM cities JOIN\
+        states ON cities.state_id = states.id ORDER BY states.id ASC"
+    cur.execute(query)
+
+    # Fetch all the rows and display the results
+    queryRows = cur.fetchall()
+    for row in queryRows:
+        print(row)
+
+    # Close the cursor and the database connection
+    cur.close()
+    connection.close()
