4b6e89270b6c06e4dfba7cf3fa5fdd3528d4902e
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-15 07:53:31 +0000

    prevent SQL injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,20 +1,35 @@
 #!/usr/bin/python3
+"""
+- takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+script should take 3 arguments: mysql username,mysql password and database name
+use the module MySQLdb (import MySQLdb)
+script should connect to a MySQL server running on localhost at port 3306
+
+"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    try:
-        args = sys.argv
-        connection = MySQLdb.connect(
-            host="localhost", port=3306, user=args[1], passwd=args[2],
-            db=args[3], charset="utf8")
-        cur = connection.cursor()
-        cur.execute('SELECT * FROM states WHERE name = %s', (args[4],))
-        queryRows = cur.fetchall()
-        for row in queryRows:
+    args = sys.argv
+
+    # Connect to the MySQL server
+    connection = MySQLdb.connect(
+        host="localhost", port=3306, user=args[1], passwd=args[2],
+        db=args[3], charset="utf8")
+
+    # Create a cursor object
+    cur = connection.cursor()
+
+    # Execute the SQL query to select states with names starting with 'N'
+    cur.execute('SELECT * FROM states WHERE name = %s', (args[4],))
+
+    # Fetch all the rows and display the results
+    queryRows = cur.fetchall()
+    for row in queryRows:
+        if row[1] == args[4]:
             print(row)
-    except MySQLdb.Error as e:
-        print(e)
-    finally:
-        cur.close()
-        connection.close()
+
+    # Close the cursor and the database connection
+    cur.close()
+    connection.close()
