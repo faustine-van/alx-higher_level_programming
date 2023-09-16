58f67d2508efd3d3a2dc8d2cb65f9d94d1ecd360
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-16 09:10:10 +0000

    updated

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+args = sys.argv
+user = args[1]
+pwd = args[2]
+db = args[3]
+
+# Create a SQLite database in memory
+engine = create_engine(
+    'mysql+mysqldb://{}:{}@localhost/{}'.format(user, pwd, db),
+    pool_pre_ping=True
+    )
+
+# Create the table
+Base.metadata.create_all(engine)
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# lists all State objects from the database hbtn_0e_6_usa
+states = session.query(State).all()
+
+for state in states:
+    print(f"{state.id}: {state.name}")
