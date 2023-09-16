e112e5ff3c5ebc4f5f819839f330ad69dd43de74
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-16 09:57:51 +0000

    fect the first one

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+if __name__ == "__main__":
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    args = sys.argv
+    user = args[1]
+    pwd = args[2]
+    db = args[3]
+
+    # Create a SQLite database in memory
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(user, pwd, db),
+        pool_pre_ping=True
+      )
+
+    # Create the table
+    Base.metadata.create_all(engine)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # lists all State objects from the database hbtn_0e_6_usa
+    states = session.query(State).first()
+
+    if states is None:
+        print("Nothing")
+
+    print(f"{states.id}: {states.name}")
