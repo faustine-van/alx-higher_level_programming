10f3fb69385057372dd02f85bdd65e1cce7f9815
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-17 10:01:42 +0000

    updated

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+ - creates the State “California” with the City “San Francisco” from the
+   database hbtn_0e_100_usa: (100-relationship_states_cities.py)
+"""
+from relationship_state import Base, State
+from relationship_city import Base, City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+
+if __name__ == "__main__":
+    args = sys.argv
+    user = args[1]
+    pwd = args[2]
+    db = args[3]
+
+    # Create a SQLite database in memory
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(user, pwd, db),
+        pool_pre_ping=True
+      )
+
+    # Create the table
+    Base.metadata.create_all(engine)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # create the State “California” with the City “San Francisco”
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+    new_state.cities.append(new_city)
+
+    session.add(new_state)
+    session.commit()
+
+    # close session
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+define class called City and define column used in into database
+
+"""
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class City(Base):
+    """define class City with column
+       - id: id of each name of state
+       - name: name of state
+       - state_id: id
+    """
+    __tablename__ = "cities"
+    id = Column("id", Integer, primary_key=True, nullable=False, unique=True)
+    name = Column("name", String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
