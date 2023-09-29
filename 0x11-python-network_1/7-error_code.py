864d11323c7497d5fdd10ad9e46265b74fc39924
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-29 21:33:40 +0000

    fixed

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    res = requests.get(sys.argv[1])
+    if res.status_code >= 400:
+        print("Error code: ", status_code)
+    print(res.text)
