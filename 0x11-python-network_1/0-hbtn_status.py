a80b1a67bc93be718356ac49fbedf7bad73c1a74
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-29 10:52:40 +0000

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,16 +2,13 @@
 """ script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-if __name__ == "__main__":
-
-    with urllib.request.urlopen(
-        'https://alx-intranet.hbtn.io/status'
-    ) as response:
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
         html = response.read()
         content = html.decode('utf-8')
         content_bytes = html
         type_content = type(html)
-    print(f"Body response:\n"
-          f"    - type: {type_content}\n"
-          f"    - content: {content_bytes }\n"
-          f"    - utf8 content: {content}")
+print(f"Body response:\n"
+      f"\t- type: {type_content}\n"
+      f"\t- content: {content_bytes }\n"
+      f"\t- utf8 content: {content}")
