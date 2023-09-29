24506faaf93b3db9be1c3152149c4d14a37c747e
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-29 21:57:46 +0000

    fixed

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
 
     res = requests.get(sys.argv[1])
     if res.status_code >= 400:
-        print("Error code: ", res.status_code)
+        print(f"Error code: {res.status_code}")
     print(res.text)
