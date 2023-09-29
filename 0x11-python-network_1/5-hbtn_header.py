453fbc67cd4a3058b72a069a352a5de59294b95c
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-29 20:26:13 +0000

    updated

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,7 +6,7 @@ import requests
 import sys
 
 
-if __name__ == "main__":
+if __name__ == "__main__":
     url = sys.argv[1]
 
     res = requests.get(url)
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response.
+"""
+import urllib.request
+from sys import argv
+
+
+if  __name__ == "__main__":
+
+    res = requests.get(argv[1])
+    data = {"email": argv[2]}
+    print("Your email is:", res.headers.get(res, data))
