6d3e134d9c5290a2bc120caf94b5b3ad2a1649cd
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-29 22:57:10 +0000

    updated

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
         with urllib.request.urlopen(url) as res:
             if res.status_code >= 400:
                 print(f"Error code: {res.status_code}")
-           else:
-               print(res.read().decode("utf-8"))
+            print(res.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
