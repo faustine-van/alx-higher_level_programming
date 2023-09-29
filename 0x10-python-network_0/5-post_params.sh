1dadb930c56974220838e2419068e9f80380778b
root <faustine-van>
2023-09-28 17:21:24 -0700

    updated

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # take URL send POST display
-curl -sX POST -d "email=test@gmail.com"&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
