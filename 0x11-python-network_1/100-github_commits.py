0b759509ba11b2028ea04d9b81fc3ff16a79b93f
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-30 21:58:56 +0000

    updated

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" list 10 commits (from the most recent to oldest) of the repository
+    “rails” by the user “rails”
+    Print all commits by: `<sha>: <author name>` (one by line)
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repos_name = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repos_name}/commits"
+    response = requests.get(url)
+    res = response.json()
+    for i in range(1, 10):
+        sha = res[i].get('sha')
+        author = res[i].get('commit').get('author').get('name')
+        print("{}: {}".format(sha, author))
