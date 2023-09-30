4c7096180c170d87d5406fa4bbde36105f71a1d0
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-30 00:48:02 +0000

    updated

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -20,6 +20,6 @@ if __name__ == "__main__":
         if not json_data:
             print("No result")
         else:
-            print(f"[{json_data.get('id')}] {json_data.get('name')}")
-    except:
+            print(f"[{json_data['id']}] {json_data['name']}")
+    except ValueError:
         print("Not a valid JSON")
