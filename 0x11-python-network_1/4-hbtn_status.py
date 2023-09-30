589eba1e5818d6291c8837f9b9611f4304000ea6
faustine-van <faustinemuhayemariya44@gmail.com>
2023-09-30 07:55:54 +0000

    updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,14 +2,10 @@
 # fetches https://alx-intranet.hbtn.io/status use the package requests
 import requests
 
-if __name__ == "__main__":
+url = "https://alx-intranet.hbtn.io/status"
+content = requests.get(url)
 
-    url = "https://alx-intranet.hbtn.io/status"
-    content = requests.get(url)
-        if content.status_code == 200:
-            print(f"Body response:\n"
-                  f"\t- type: {type(content.text)}\n"
-                  f"\t- content: {content.text}"
-                )
-        else:
-            print("failed")
+print(f"Body response:\n"
+      f"\t- type: {type(content.text)}\n"
+      f"\t- content: {content.text}"
+      )
