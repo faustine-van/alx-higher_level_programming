7bd2c63b9afc8c22ea68cd2b79b203bf52478ce8
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-24 14:15:41 +0300

    updated

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 // display the status code of a GET request.
-const request = require('request')
-const process = require('process')
-const args = process.argv
-const url = args[2]
+const request = require('request');
+const process = require('process');
+const args = process.argv;
+const url = args[2];
 
-request.get(url, function (error, response) {
+request.get(url, (error, response) => {
   // Print the response status code if a response was received
   if (error) {
     console.error(error);
     return;
   }
-  console.log('code: ', response.statusCode); 
+  console.log('code: ', response.statusCode);
 });
