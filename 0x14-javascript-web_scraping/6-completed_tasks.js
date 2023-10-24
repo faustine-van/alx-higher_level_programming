3b842d258f17fcffd6e61adfe1e883eafae38195
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-24 22:57:57 +0300

    updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,38 +5,19 @@ const process = require('process');
 const args = process.argv;
 const url = args[2];
 
-const urlUser = 'https://jsonplaceholder.typicode.com/users';
-
-request(urlUser, (err, res, body1) => {
+request(url, (err, res, body) => {
   if (err) {
     console.log(err);
   }
 
-  const jsonUser = JSON.parse(body1);
+  const jsonTodo = JSON.parse(body);
   const userCounts = {}; // Create an object to store user counts
 
-  function processUser (index) {
-    if (index < jsonUser.length) {
-      const userId = jsonUser[index].id;
-      let count = 0;
-      const urlTodo = url + `?userId=${userId}`;
-
-      request(urlTodo, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        }
-        const jsonTodo = JSON.parse(body);
-        for (const number in jsonTodo) {
-          if (jsonTodo[number].completed === true) {
-            count += 1;
-          }
-        }
-        userCounts[userId] = count; // store the counts for this user id
-        processUser(index + 1); // move to next user
-      });
-    } else {
-      console.log(userCounts);
+  for (const todo of jsonTodo) {
+    if (todo.completed) {
+      const userId = todo.userId;
+      userCounts[userId] = (userCounts[userId] || 0) + 1;
     }
   }
-  processUser(0); // // Start processing from the first user
+  console.log(userCounts);
 });
