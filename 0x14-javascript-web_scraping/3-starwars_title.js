1a2b9d643545e2cb3494438748540385f8848212
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-24 15:23:14 +0300

    updated

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// title of a Star Wars movie where the episode number matches a given integer
+const request = require('request');
+const process = require('process');
+const args = process.argv;
+const id = parseInt(args[2]);
+
+const url = `https://swapi-api.alx-tools.com/api/film/${id}`;
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const json = JSON.parse(body);
+  console.log(json.title);
+});
