6f92ac3c8e1b1ffacc12e790a1a89128a84deb06
faustine-van <faustinemuhayemariya44@gmail.com>
2023-12-19 08:48:36 +0200

    updated

---
+++ learn/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HelloWorld from './Components/HelloWorld'
+//import Counter from './Components/Counter'
+
+function App() {
+  return (
+    <div>
+      <HelloWorld name="Sugira" />
+    </div>
+  );
+}
+
+export default App;
